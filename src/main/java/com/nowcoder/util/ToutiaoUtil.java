5eec850f3174e85894a02c5a144262356f759648
zhenglingxiao <zlxyves@outlook.com>
2019-07-02 16:44:50 +0800

    第五次课完成,实现课程内容的所有功能

--- src/main/java/com/nowcoder/util/ToutiaoUtil.java
+++ src/main/java/com/nowcoder/util/ToutiaoUtil.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class ToutiaoUtil {
     private static final Logger logger = LoggerFactory.getLogger(ToutiaoUtil.class);
 
+    public static String QINIU_DOMAIN_PREFIX = "http://pu086vr5m.bkt.clouddn.com/";
     public static String TOUTIAO_DOMAIN = "http://127.0.0.1:8080/";
     public static String IMAGE_DIR = "D:/upload/";
     public static String[] IMAGE_FILE_EXT = new String[]{"png", "bmp","jpg","jpeg"};
